df7d9592b9e607e6b0d27e5b9729a5d340899f68
Esther Reeves <49820664+Estherreeves@users.noreply.github.com>
2020-05-16 21:49:10 -0500

    Update index.js

--- About-Page/pages/index.js
+++ About-Page/pages/index.js
@@ -13,11 +13,11 @@ export default function Home() {
         <p>
           An online blog site for Christians to meet, share their God moments, and leave prayer requests.
           Today socity with all of our socal distancing, and socal media platforms available out there. Kairos 
-          moments has designed a platform for those who are looking for and who have experienced their own God
+          Coders has designed a platform for those who are looking for and who have experienced their own God
           moments to share them with others. One of the most powerful forces is the "Word of Our Testimony" 
           combined with the "Blood of the Lamb" and "not loving our own lives" results in overcoming our oldest
-          enemy. Revelation 12:11 "And they overcame him by the blook of the Lamb, and by  the word of their 
-          testimony; and they loved not their lives unto death"
+          enemy. Revelation 12:11 "And they overcame him by the blood of the Lamb, and by  the word of their 
+          testimony; and they loved not their own lives unto death"
           </p>
       </section>
     </Layout>
